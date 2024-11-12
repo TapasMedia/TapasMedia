40f33c021e4b80e3a2d66ccc6086e87cb6a19ea7
Satwik Gupta <satwikgupta1801@gmail.com>
2024-11-13 00:18:28 +0530

    Update JoinButton to open Calendly link in a new tab

--- src/components/ui/custom/JoinButton.jsx
+++ src/components/ui/custom/JoinButton.jsx
@@ -5,7 +5,12 @@ const JoinButton = ({ children }) => {
   return (
     <div className=" border border-white/[0.2] rounded-full bg-[#242424] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[500] hover:scale-105 ">
       <ShimmerButton
-        onClick={() => (window.location.href = "/#subscribe")}
+        onClick={() => {
+          window.open(
+            "https://calendly.com/ap654-undead/30min?hide_gdpr_banner=1",
+            "_blank"
+          );
+        }}
         className="shadow-2xl transition-all duration-300 "
         shimmerColor={["#0ea5e9", "#FE8FB5", "#FFBE7B", "#FFBE7B"]}
         shimmerSize="0.20em"
